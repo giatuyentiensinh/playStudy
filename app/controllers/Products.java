d26f46c7767a632de0446b0d308bb60c978a49fe
Tuyen-ng <giatuyentiensinh@gmail.com>
2015-06-13 15:01:21 +0700

    up load project and started methods list, details. save, newProduct, and configured routes

---
+++ app/controllers/Products.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Products extends Controller{
+
+	public Result list() {
+		return TODO;
+	}
+	
+	public Result newProduct() {
+		return ok();
+	}
+	
+	public Result details(String ean) {
+		return ok();
+	}
+	
+	public Result save() {
+		return ok();
+	}
+}
