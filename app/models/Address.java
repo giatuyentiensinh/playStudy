68901f6650c0b24ae3110627fdf88a922c52d410
Tuyen-ng <giatuyentiensinh@gmail.com>
2015-06-14 22:08:37 +0700

    done mapping Warehouse-Address

---
+++ app/models/Address.java
@@ -0,0 +1,23 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.avaje.ebean.Model;
+
+@Entity
+public class Address extends Model {
+
+	@Id
+	public Long id;
+	@OneToOne(mappedBy = "address")
+	public Warehouse warehouse;
+
+	public String street;
+	public String number;
+	public String postalCode;
+	public String city;
+	public String country;
+
+}
--- app/models/Warehouse.java
+++ app/models/Warehouse.java
@@ -6,13 +6,18 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.avaje.ebean.Model;
 
 @Entity
-public class Warehouse {
+public class Warehouse extends Model{
 
 	@Id
 	public Long id;
 	public String name;
+	@OneToOne
+	public Address address;
 	@OneToMany(mappedBy = "warehouse")
 	public List<StockItem> stock = new ArrayList<StockItem>();
 
